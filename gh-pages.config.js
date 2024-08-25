ce4cec8c491dd71890266c1aa4add30a3a1b1d09
moeez7978 <moeez7978911@gmail.com>
2024-08-25 14:51:15 +0500

    installed gh-pages

---
+++ gh-pages.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // Path to your built website (e.g., dist, build, or public)
+    src: 'dist',
+    // Branch to deploy to (default: gh-pages)
+    branch: 'gh-pages',
+  };
+  
